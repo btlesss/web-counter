5c49838956a5d6850e3c6e591c82be45e78a8af3
Alex <zovitemenakrutoj@gmail.com>
2024-01-04 17:59:28 +0500

    thousands separator, counter view

--- app/routers/frontend.py
+++ app/routers/frontend.py
@@ -2,10 +2,16 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from utils import RedisDB, group_from_redis
-from dtypes import RedisCounter, Counter, RedisGroup, Group
+from utils import RedisDB, group_from_redis, counter_from_redis
+from dtypes import RedisCounter, Counter, RedisGroup, Group, CounterTimings
 
 templates = Jinja2Templates(directory="templates")
+
+def pretty_number(input):
+    """Custom filter"""
+    return "{:,}".format(input)
+
+templates.env.filters["pretty"] = pretty_number
 
 router = APIRouter(
     tags=["user interface"]
@@ -26,4 +32,11 @@ async def view_group(request: Request, group_id: str):
 
 @router.get("/count/{counter_id}", response_class=HTMLResponse, tags=["user interface"])
 async def view_counter(request: Request, counter_id: str):
-    return templates.TemplateResponse(name="counter.html", context={"request": request})
+    async with RedisDB() as db:
+        counter_raw = await db.hgetall(f"counter:{counter_id}")
+        if counter_raw == {}:
+            counter = Counter(name="Counter not found", value=0, timings=CounterTimings())
+        else:
+            counter = await counter_from_redis(RedisCounter(**counter_raw), db)
+    
+    return templates.TemplateResponse(name="counter.html", context={"request": request, "counter": counter})
